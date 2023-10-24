46d56df0d20437644c3b3405805ef66d40512783
sanjaykrishnan <er.sanjaykrishnan@gmail.com>
2023-10-24 20:15:53 +0530

    signinwithemailand passwrd added

--- signin/signin.js
+++ signin/signin.js
@@ -1,18 +1,32 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCioblfAsN0vL2weeRR1ul8278xzjBz2Rc",
-  authDomain: "movies-99135.firebaseapp.com",
-  databaseURL: "https://movies-99135-default-rtdb.firebaseio.com",
-  projectId: "movies-99135",
-  storageBucket: "movies-99135.appspot.com",
-  messagingSenderId: "981899759318",
-  appId: "1:981899759318:web:38e8de34df59aa06b44377"
-};
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCioblfAsN0vL2weeRR1ul8278xzjBz2Rc",
+//   authDomain: "movies-99135.firebaseapp.com",
+//   databaseURL: "https://movies-99135-default-rtdb.firebaseio.com",
+//   projectId: "movies-99135",
+//   storageBucket: "movies-99135.appspot.com",
+//   messagingSenderId: "981899759318",
+//   appId: "1:981899759318:web:38e8de34df59aa06b44377"
+// };
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// import {
+//   getAuth,
+//   signInWithEmailAndPassword,
+// } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import {
   getAuth,
   signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
-
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkEgmgMAKS4tRbCaYR3MhHTlT11eKR4Aw",
+  authDomain: "moviedb-48bbf.firebaseapp.com",
+  databaseURL: "https://moviedb-48bbf-default-rtdb.firebaseio.com",
+  projectId: "moviedb-48bbf",
+  storageBucket: "moviedb-48bbf.appspot.com",
+  messagingSenderId: "338786943064",
+  appId: "1:338786943064:web:c51941f2e6e5e54b6c4a2d"
+};
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
 const email = document.getElementById("signInEmailId");
@@ -27,6 +41,7 @@ document.getElementById("form").addEventListener("submit", (e) => {
     signInWithEmailAndPassword(auth, email.value, password.value)
       .then((userDetails) => {
         alert("success");
+        window.location.href="../profile/profile.html";
       })
       .catch((error) => {
         alert(error.message);
